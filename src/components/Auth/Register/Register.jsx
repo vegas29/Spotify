f9d694e8d93a9105ff80d4f57037f141fc9d784d
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-05-27 18:45:50 -0500

    Validation on register screen, done

--- src/components/Auth/Register/Register.jsx
+++ src/components/Auth/Register/Register.jsx
@@ -1,36 +1,62 @@
-import { Button, Form, Icon } from "semantic-ui-react";
+import { useState } from "react";
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from "./Register.data";
+import { Form, Icon } from "semantic-ui-react";
 import "./Register.scss";
 
 export const Register = ({ openLogin, goBack }) => {
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: validationSchema(),
+        validateOnChange: false,
+        onSubmit: (formValue) => {
+            console.log(formValue)
+        },
+      });
+
+
     return (
         <div className="register-form">
 
             <h1>Start a listen with a free spotify account</h1>
 
-            <Form>
+            <Form onSubmit={formik.handleSubmit}>
                 <Form.Input 
                     type="text" 
                     placeholder="Email" 
                     icon="mail outline" 
-                    // error={true}
+                    name="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                    error={formik.errors.email}
                 />
 
                 <Form.Input 
                     type="password" 
                     placeholder="Password" 
                     icon={<Icon name="eye" link onClick={ () => console.log('Show Password')} />}
+                    name="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    error={formik.errors.password}
                 />
 
                 <Form.Input
                     type="text"
                     placeholder="Username"
                     icon="user circle outline"
+                    name="username"
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                    error={formik.errors.username}
                 />
 
                 <Form.Button
                     type="submit"
                     primary
                     fluid
+                    loading={formik.isSubmitting}
                 >
                     Continue
                 </Form.Button>    
