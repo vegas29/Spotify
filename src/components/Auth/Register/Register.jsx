493ed1dc8e3db60237ff6356a4cffddab96742bf
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-05-27 18:21:07 -0500

    Screen register, done

--- src/components/Auth/Register/Register.jsx
+++ src/components/Auth/Register/Register.jsx
@@ -1,12 +1,52 @@
-import { Button } from "semantic-ui-react";
+import { Button, Form, Icon } from "semantic-ui-react";
+import "./Register.scss";
 
 export const Register = ({ openLogin, goBack }) => {
     return (
-        <div style={{backgroundColor: '#f00'}}>
-            <h1>Register</h1>
+        <div className="register-form">
 
-            <Button primary onClick={openLogin}>Login</Button>
-            <Button secondary onClick={goBack}>Go back</Button>
+            <h1>Start a listen with a free spotify account</h1>
+
+            <Form>
+                <Form.Input 
+                    type="text" 
+                    placeholder="Email" 
+                    icon="mail outline" 
+                    // error={true}
+                />
+
+                <Form.Input 
+                    type="password" 
+                    placeholder="Password" 
+                    icon={<Icon name="eye" link onClick={ () => console.log('Show Password')} />}
+                />
+
+                <Form.Input
+                    type="text"
+                    placeholder="Username"
+                    icon="user circle outline"
+                />
+
+                <Form.Button
+                    type="submit"
+                    primary
+                    fluid
+                >
+                    Continue
+                </Form.Button>    
+            </Form>
+
+            <div className="register-form__options">
+                <p onClick={goBack}>
+                    <Icon name="angle left"/>
+                    Go back
+                </p>
+                <p>
+                    ¿Already account?
+                    <span onClick={openLogin}>Sign In</span>
+                </p>
+            </div>
+            
         </div>
     )
 }
