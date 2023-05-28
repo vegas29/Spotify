bcb9cd4056be050859e6578b675197402e597e58
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-05-27 19:08:43 -0500

    Logout is done

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,7 +1,20 @@
+import { Button } from "semantic-ui-react"
+import { Auth } from "../../api";
 
 
 export const HomePage = () => {
+
+    const { logout } = new Auth();
+    
     return (
-        <div>HomePage</div>
+        <div>   
+            HomePage
+            <Button
+                onClick={ () => logout()}
+                color="red"
+            >
+                Cerrar Sesion
+            </Button>
+        </div>
     )
 }
